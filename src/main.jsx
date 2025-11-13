45e3f287425f073cd09949417fd175d72190b279
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2025-11-13 10:20:28 +0600

    install tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../src/assets/styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    
   </StrictMode>,
 )
