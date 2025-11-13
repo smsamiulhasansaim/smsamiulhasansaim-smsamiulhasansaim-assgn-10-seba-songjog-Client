0d82a7dee5819534af6b6afc33c361de6f94337b
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2025-11-13 10:22:15 +0600

    install React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../src/assets/styles/index.css'
+import { router } from './Routes/AppRoutes'
+import { RouterProvider } from "react-router/dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+  <RouterProvider router={router} />,
   </StrictMode>,
 )
